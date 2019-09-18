d172c70cb3676f3d2e48fa2d1d2424ace706aba5
Alethia Quintero <alethiaq22@gmail.com>
2019-09-18 13:41:37 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,25 @@
-require './config/environment'
+require "./config/environment"
 
 class ApplicationController < Sinatra::Base
-
+  register Sinatra::ActiveRecordExtension
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "tweeter"
   end
 
+  get "/" do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
